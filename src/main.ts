ec8625e5a6c78f6d14d9c9b2d7d259bdd20772eb
cqfQuant <speedupurlearning@gmail.com>
2024-06-29 20:46:59 +0530

    ADD port

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
-bootstrap();
+bootstrap().then(() =>
+  console.log(`Application is running on: ${process.env.PORT || 3000}`),
+);
